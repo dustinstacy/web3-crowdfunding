473270f947f2d069d857384177daf3ed74104c1e
Dustin Stacy <dustin.stacy@outlook.com>
2023-01-12 10:23:15 -0500

    Add Context and Web3 form submission handling

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -2,12 +2,13 @@ import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ethers } from 'ethers';
 
+import { useStateContext } from '../context';
 import { money } from '../assets';
-import { CustomButton } from '../components';
+import { CustomButton, FormField } from '../components';
 import { checkIfImage } from '../utils';
-import { FormField } from '../components';
 
 const CreateCampaign = () => {
+  const { createCampaign } = useStateContext();
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false)
   const [form, setForm] = useState({
@@ -23,8 +24,20 @@ const CreateCampaign = () => {
     setForm({ ...form, [fieldName]: e.target.value })
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    checkIfImage(form.image, async (exists) => {
+      if(exists) {
+        setIsLoading(true);
+        await createCampaign({ ...form, target: ethers.utils.parseUnits(form.target, 18) })
+        setIsLoading(false);
+        navigate('/');
+      } else {
+        alert('Provide valid image URL');
+        setForm({ ...form, image: '' });
+      }
+    })
 
     console.log(form);
   }
